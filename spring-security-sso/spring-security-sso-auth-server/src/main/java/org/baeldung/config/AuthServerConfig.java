77ddbd19f6e17b6ca61f284d7f41aa491e374fac
Johnson Fu <johnson.fu@gmail.com>
2019-08-12 23:45:41 +0800

    update

--- spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/AuthServerConfig.java
+++ spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/AuthServerConfig.java
@@ -1,12 +1,21 @@
 package org.baeldung.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 
 @Configuration
@@ -15,6 +24,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     
     @Autowired    
     private BCryptPasswordEncoder passwordEncoder;
+
+    // h2 datasource
+    @Autowired
+    private DataSource dataSource;
     
     @Override
     public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
@@ -22,18 +35,66 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
             .checkTokenAccess("isAuthenticated()");
     }
 
+    // create the in memory token store
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new InMemoryTokenStore();
+//    }    
+    
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }    
+    
+    
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+    
+    /**
+     * config to use in memory token store
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(tokenStore())
+                .authenticationManager(authenticationManager);
+    }    
+
     @Override
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+    	
+    	/** data example
+    	 *  https://pattern-match.com/blog/2018/10/17/springboot2-with-oauth2-integration/
+    	 *  
+    	 *  
+	        clients
+	        .inMemory()
+	        .withClient(CLIENT_ID)
+	        .secret(CLIENT_SECRET)
+	        .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN)
+	        .scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+	        .accessTokenValiditySeconds(VALID_FOREVER)
+	        .refreshTokenValiditySeconds(VALID_FOREVER);
+        */    	
+    	
         clients.inMemory()
-            .withClient("SampleClientId")
+            .withClient("app1")
             .secret(passwordEncoder.encode("secret"))
             .authorizedGrantTypes("authorization_code")
             .scopes("user_info")
             .autoApprove(true)
-            .redirectUris("http://localhost:8082/ui/login","http://localhost:8083/ui2/login","http://localhost:8082/login","http://www.example.com/")
-        // .accessTokenValiditySeconds(3600)
-        ; // 1 hour
+            .redirectUris("http://localhost:8082/ui/login")
+            .accessTokenValiditySeconds(3600)    
+            .and()
+	        .withClient("app2")
+	        .secret(passwordEncoder.encode("secret"))
+	        .authorizedGrantTypes("authorization_code")
+	        .scopes("user_info")
+	        .autoApprove(true)
+	        .redirectUris("http://localhost:8083/ui2/login")
+	        .accessTokenValiditySeconds(3600)                                    
+        ;
+        
     }
-
 
 }
--- spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/SecurityConfig.java
+++ spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package org.baeldung.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -14,24 +16,61 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception { // @formatter:off
-        http.requestMatchers()
-            .antMatchers("/login", "/oauth/authorize")
-            .and()
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .formLogin()
-            .permitAll()
-            .and().csrf().disable();
+        http
+        	.requestMatchers()        	        	
+            	.antMatchers("/login", "/oauth/authorize","/exit","/h2","/h2/**")
+            	.and()
+            	.authorizeRequests()            
+            	.anyRequest()
+            	.authenticated()
+//            .and()
+//            	.authorizeRequests()
+//            	.antMatchers("/h2/**").permitAll()
+        	.and()
+            	.formLogin()
+            	.permitAll()            
+            .and().csrf().disable()            
+            ;
+    	
+    	
+//        http
+//    	.authorizeRequests()
+//    		.antMatchers("/h2_console/**").permitAll()
+//    		.and()
+//    	.authorizeRequests()        	        	
+//        	.antMatchers("/login", "/oauth/authorize","/exit")
+//        	.authenticated()
+//    	.and()
+//        	.formLogin()
+//        	.permitAll()            
+//        .and().csrf().disable()            
+//        ;    	
+//        
+//        http.headers().frameOptions().disable();
+        
+
+    	
+ //        http.authorizeRequests()        
+//        .antMatchers("/h2_console/**").permitAll();
+//		http.csrf().disable();
+		http.headers().frameOptions().disable();
+        
     } // @formatter:on
+    
+   
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }    
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
         auth.inMemoryAuthentication()
-            .withUser("john")
-            .password(passwordEncoder().encode("123"))
+            .withUser("johnson")
+            .password(passwordEncoder().encode("abcd1234"))
             .roles("USER");
+        
     } // @formatter:on
 
     @Bean
