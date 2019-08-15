ee7bb82c86a4bc6006abef80380b7ca90a26308a
Johnson Fu <johnson.fu@gmail.com>
2019-08-15 17:12:17 +0800

    try custom login page

--- spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/WebSecurityConfiguration.java
+++ spring-security-sso/spring-security-sso-auth-server/src/main/java/org/baeldung/config/WebSecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebSecurity
 @Configuration
@@ -41,6 +42,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return passwordEncoder;
     }
     
+//    @Bean
+//    public ServletContextTemplateResolver templateResolver() {
+//    	
+//        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(null);
+//        resolver.setPrefix("/WEB-INF/views");
+//        resolver.setSuffix(".html"); // here
+//        resolver.setTemplateMode("HTML5");
+//        resolver.setOrder(1);
+//        return resolver;
+//    }    
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception { // @formatter:off
     	
@@ -52,10 +64,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         // example
         // define /test end point will be intercepted
         http.requestMatchers().antMatchers("/test");
+        http.requestMatchers().antMatchers("/moon");
+        http.requestMatchers().antMatchers("/sun");
+        http.requestMatchers().antMatchers("/resources/templates/**");
+        http.requestMatchers().antMatchers("/resources/**");
         
         // such that we could assign the permission
         http.authorizeRequests().antMatchers("/test").permitAll();
+        http.authorizeRequests().antMatchers("/moon").permitAll();
+        http.authorizeRequests().antMatchers("/sun").permitAll();
         http.authorizeRequests().antMatchers("/h2/**").permitAll();
+        http.authorizeRequests().antMatchers("/resources/templates/**").permitAll();
+        http.authorizeRequests().antMatchers("/resources/**").permitAll();
 
         // all other requests must be authenticated
         http
@@ -63,8 +83,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             	.anyRequest()
             	.authenticated();
                         
-        // enable default form login
+        // form based authentication is supported
         http.formLogin().permitAll();
+        //http.formLogin().loginPage("/login").permitAll();
+        //http.formLogin().loginProcessingUrl("/perform_login");
         
         
         // create custom login form 
@@ -102,7 +124,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }    
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
